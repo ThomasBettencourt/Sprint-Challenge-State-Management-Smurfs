d241b1b06a9a69387ee0bf65d0edc7d4658d6557
ThomasBettencourt <thomasbetten.cs@gmail.com>
2020-05-01 21:00:46 -0500

    code base built

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"; 
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(applyMiddleware(logger, thunk))
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, document.getElementById("root"));
